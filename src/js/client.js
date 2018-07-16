c76f4b9fc20c71a8a775239deb4c61c354f3ee2b
Unknown <caarroyavepadilla@gmail.com>
2018-07-16 14:01:59 -0600

    progreso

--- src/js/client.js
+++ src/js/client.js
@@ -1,108 +1,141 @@
-import expect from 'expect';
-import { createStore } from 'redux';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Immutable from 'immutable';
+const estado = {
+  turno: 1,
+  tablero: [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
-// Javascript the good parts
+}
+const render = lstate =>{
+  const title = document.createElement('h1');
+title.className = 'title-tag';
+title.innerHTML = 'TOTITO';
+root.className='root-tag';
 
-const Counter = ({ value, incrementAction, decrementAction, removeAction }) => (
-  <div>
-    <h1>{ value }</h1>
-    <button onClick={ incrementAction }>+</button>
-    <button onClick={ decrementAction }>-</button>
-    <button onClick={ removeAction }>x</button>
-  </div>
-);
+const player = document.createElement('h1');
+player.className = 'player-tag';
+player.innerHTML = 'Turno Jugador X';
 
-const CounterList = ({ list }) => (
-  <div>
-    {
-      list.map(
-        (value, i) => (
-          <Counter
-            key={ i }
-            value={ value }
-            index={ i }
-            incrementAction={
-              () => store.dispatch({
-                type: 'INCREMENT',
-                payload: { index: i }
-              })
-            }
-            decrementAction={
-              () => store.dispatch({
-                type: 'DECREMENT',
-                payload: { index: i }
-              })
-            }
-            removeAction={
-              () => store.dispatch({
-                type: 'REMOVE_COUNTER',
-                payload: {
-                  index: i
-                }
-              })
-            }
-          />
-        )
-      )
+
+const btnReinciar = document.createElement('button');
+btnReinciar.className = 'btn';
+btnReinciar.innerHTML = 'Reinciar'
+
+const tablero = document.createElement('div');
+tablero.className = 'tablero-contenedor';
+console.log(estado.tablero);
+
+const winner = document.createElement('h1');
+winner.className = 'winner-tag';
+
+for (let i = 0; i < 3; i++) {
+  const filas = document.createElement('div');
+  filas.className = 'fila-contenedor';
+  tablero.appendChild(filas);
+  for (let j = 0; j < 3; j++) {
+    const columnas = document.createElement('div');
+    columnas.className = 'columna-contenedor';
+    columnas.innerHTML = '<img src = "https://www.shareicon.net/data/128x128/2016/10/01/837948_miscellaneous_512x512.png">'
+    columnas.onclick = function () {
+      estado.turno++;
+      if (estado.turno % 2 == 0) {
+        if (estado.tablero[i][j] == 0) {
+          estado.tablero[i][j] = 1;
+          columnas.innerHTML = '<img src = "https://vignette.wikia.nocookie.net/mysingingmonsters/images/2/24/Red-x.png/revision/latest?cb=20171113115328">'
+          player.innerHTML = 'Turno jugador O';
+        }
+
+      } else {
+        if (estado.tablero[i][j] == 0) {
+          estado.tablero[i][j] = 2;
+          columnas.innerHTML = '<img src = "https://upload.wikimedia.org/wikipedia/commons/0/05/Red_circle.svg">'
+          player.innerHTML = 'Turno jugador X';
+        }
+      }
+      //primer estado win x
+      if (estado.tablero[0][0] == 1 & estado.tablero[1][0] == 1 & estado.tablero[2][0] == 1) {
+        winner.innerHTML = 'Gano Jugador X';
+      }
+      //segundo estado win x
+      if (estado.tablero[0][0] == 1 & estado.tablero[0][1] == 1 & estado.tablero[0][2] == 1) {
+        winner.innerHTML = 'Gano Jugador X';
+      }
+      //tercer estado win x
+      if (estado.tablero[1][0] == 1 & estado.tablero[1][1] == 1 & estado.tablero[1][2] == 1) {
+        winner.innerHTML = 'Gano Jugador X';
+      }
+      //cuarto estado win x
+      if (estado.tablero[2][0] == 1 & estado.tablero[2][1] == 1 & estado.tablero[2][2] == 1) {
+        winner.innerHTML = 'Gano Jugador X';
+      }
+      //quinto estado win x
+      if (estado.tablero[0][1] == 1 & estado.tablero[1][1] == 1 & estado.tablero[2][1] == 1) {
+        winner.innerHTML = 'Gano Jugador X';
+      }
+      //sexto estado win x
+      if (estado.tablero[0][2] == 1 & estado.tablero[1][2] == 1 & estado.tablero[2][2] == 1) {
+        winner.innerHTML = 'Gano Jugador X';
+      }
+      //septimo estado win x
+      if (estado.tablero[0][0] == 1 & estado.tablero[1][1] == 1 & estado.tablero[2][2] == 1) {
+        winner.innerHTML = 'Gano Jugador X';
+      }
+      //octavo estado win x
+      if (estado.tablero[0][2] == 1 & estado.tablero[1][1] == 1 & estado.tablero[2][0] == 1) {
+        winner.innerHTML = 'Gano Jugador X';
+      }
+      //primer estado win o
+      if (estado.tablero[0][0] == 2 & estado.tablero[1][0] == 2 & estado.tablero[2][0] == 2) {
+        winner.innerHTML = 'Gano Jugador O';
+      }
+      //segundo estado win o
+      if (estado.tablero[0][0] == 2 & estado.tablero[0][1] == 2 & estado.tablero[0][2] == 2) {
+        winner.innerHTML = 'Gano Jugador O';
+      }
+      //tercer estado win o
+      if (estado.tablero[1][0] == 2 & estado.tablero[1][1] == 2 & estado.tablero[1][2] == 2) {
+        winner.innerHTML = 'Gano Jugador O';
+      }
+      //cuarto estado win o
+      if (estado.tablero[2][0] == 2 & estado.tablero[2][1] == 2 & estado.tablero[2][2] == 2) {
+        winner.innerHTML = 'Gano Jugador O';
+      }
+      //quinto estado win o
+      if (estado.tablero[0][1] == 2 & estado.tablero[1][1] == 2 & estado.tablero[2][1] == 2) {
+        winner.innerHTML = 'Gano Jugador O';
+      }
+      //sexto estado win o
+      if (estado.tablero[0][2] == 2 & estado.tablero[1][2] == 2 & estado.tablero[2][2] == 2) {
+        winner.innerHTML = 'Gano Jugador O';
+      }
+      //septimo estado win o
+      if (estado.tablero[0][0] == 2 & estado.tablero[1][1] == 2 & estado.tablero[2][2] == 2) {
+        winner.innerHTML = 'Gano Jugador O';
+      }
+      //octavo estado win o
+      if (estado.tablero[0][2] == 2 & estado.tablero[1][1] == 2 & estado.tablero[2][0] == 2) {
+        winner.innerHTML = 'Gano Jugador O';
+      }
+
     }
-    <button onClick={ () => store.dispatch({ type: 'ADD_COUNTER' }) }>Add counter</button>
-  </div>
-);
-
-const validateIndex = (index, list) => 0 <= index && index < list.size;
-
-// Reducer
-const counterList = (state = Immutable.List.of(), action) => {
-
-  if(typeof action.payload !== 'undefined'){
-    var { index } = action.payload;
+    filas.appendChild(columnas);
+    btnReinciar.onclick = function(){
+      estado.tablero = [[0,0,0],[0,0,0],[0,0,0]];
+      winner.innerHTML = ' ';
+      root.innerHTML = null;
+      render(estado);
+      
+      }
+    }
   }
+  root.appendChild(title);
+  root.appendChild(tablero);
+  root.appendChild(player);
+  root.appendChild(winner);
+  root.appendChild(btnReinciar);
 
-  switch(action.type){
-    case 'ADD_COUNTER':
-      return state.push(0);
-
-    case 'REMOVE_COUNTER':
-
-      if(validateIndex(index, state)){
-        return state.delete(index);
-      }
-
-      return state;
-
-    case 'INCREMENT':
-
-      if(validateIndex(index, state)){
-        return state.update(index, (v) => v + 1);
-      }
-
-      return state;
-
-    case 'DECREMENT':
-
-      if(validateIndex(index, state)){
-        return state.update(index,  (v) => v - 1);
-      }
-
-      return state;
-
-    default:
-      return state;
-  }
 }
+render(estado);
 
-// createStore: reducer --> store
-const store = createStore(counterList);
 
-const render = () => {
-  ReactDOM.render(
-    <CounterList list={ store.getState() } />,
-    document.getElementById('root')
-  )
-}
 
-store.subscribe(render);
-render();
+
+
+
